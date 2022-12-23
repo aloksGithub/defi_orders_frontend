d9fe77a9a56bc9fd7eff05bab9a89c78ed547959
Alok <alokgithub@gmail.com>
2022-12-23 09:51:21 +0700

    Amplify api fix

--- next.config.js
+++ next.config.js
@@ -5,7 +5,10 @@ const nextConfig = {
   env: {
     infuraKey: process.env.INFURA_KEY,
     alchemyKey: process.env.ALCHEMY_KEY,
-    COVALENT_KEY: process.env.COVALENT_KEY
+    COVALENT_KEY: process.env.COVALENT_KEY,
+    1: process.env['1'],
+    56: process.env['56'],
+    97: process.env['97']
   },
 }
 
